07b4af2f9bd1d4b4007c2641024e51165e306a47
SeanMEpi <seanmorrison112358@gmail.com>
2014-11-06 13:55:39 -0800

    Update soundbox.js

--- js/soundbox.js
+++ js/soundbox.js
@@ -129,8 +129,6 @@ var audioContext = new webkitAudioContext();
 var ballSound0 = Object.create(Sound);
 ballSound0.initialize();
 
-
-
 // jQuery EventListeners
 $('#gravOnButton').click(function(){
   gravity = 0.2;
@@ -139,49 +137,3 @@ $('#gravOnButton').click(function(){
 $('#gravOffButton').click(function(){
   gravity = 0.0;
 });
-
-
-
-// var clicks = 0;
-// var lastClick = [0, 0];
-
-// document.getElementById('canvas').addEventListener('click', drawLine, false);
-
-// function getCursorPosition(e) {
-//     var x;
-//     var y;
-
-//     if (e.pageX != undefined && e.pageY != undefined) {
-//         x = e.pageX;
-//         y = e.pageY;
-//     } else {
-//         x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-//         y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-//     }
-
-//     return [x, y];
-// }
-
-// function drawLine(e) {
-//     context = this.getContext('2d');
-
-//     x = getCursorPosition(e)[0] - this.offsetLeft;
-//     y = getCursorPosition(e)[1] - this.offsetTop;
-
-//     if (clicks != 1) {
-//         clicks++;
-//     } else {
-//         context.beginPath();
-//         context.moveTo(lastClick[0], lastClick[1]);
-//         context.lineTo(x, y, 6);
-
-//         context.strokeStyle = '#000000';
-//         context.stroke();
-
-//         clicks = 0;
-//     }
-
-//     lastClick = [x, y];
-// };
-
-
